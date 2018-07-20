4af1f38e58330a2596217815a39b397238c9b7aa
Rainer Erich Scheichelbauer <res@glyphsapp.com>
2018-07-20 17:46:26 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SnapKiller
+
 Glyphs.app Plug-in: reduces amount of magnetic snapping in edit view in Glyphs 2.5
+
+### License
+
+Copyright 2018 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt) and Jan Gerner (@yanone).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ SnapKiller.glyphsPlugin/Contents/Resources/plugin.py
@@ -0,0 +1,79 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	General Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/General%20Plugin
+#
+#
+###########################################################################################################
+
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class SnapKiller(GeneralPlugin):
+	def settings(self):
+		self.name = Glyphs.localize({
+			'en': u'Kill Snapping', 
+			'de': u'Einrasten unterdrücken',
+		})
+	
+	def start(self):
+		self.isKilling = False
+		self.hasNotification = False
+		
+		Glyphs.registerDefault("com.mekkablue.SnapKiller.state", False)
+
+		self.menuItem = NSMenuItem(self.name, self.toggleSnapKill)
+		Glyphs.menu[EDIT_MENU].append(self.menuItem)
+		
+		self.setSnapKillState(self.getSnapKillState())
+	
+	def __del__(self):
+		try:
+			if self.hasNotification:
+				Glyphs.removeCallback(self.killSnaps, DRAWFOREGROUND)
+				self.hasNotification = False
+		except:
+			# exit gracefully, but do report:
+			import traceback
+			print traceback.format_exc()
+	
+	def toggleSnapKill(self, sender):
+		self.setSnapKillState(not self.getSnapKillState())
+	
+	def getSnapKillState(self):
+		return Glyphs.boolDefaults["com.mekkablue.SnapKiller.state"]
+	
+	def setSnapKillState(self, state):
+		Glyphs.boolDefaults["com.mekkablue.SnapKiller.state"] = bool(state)
+		if not state:
+			if self.hasNotification:
+				Glyphs.removeCallback(self.killSnaps, DRAWFOREGROUND)
+				self.hasNotification = False
+		else:
+			if not self.hasNotification:
+				Glyphs.addCallback(self.killSnaps, DRAWFOREGROUND)
+				self.hasNotification = True
+		
+		currentState = ONSTATE if state else OFFSTATE
+		self.menuItem.setState_(currentState)
+	
+	def killSnaps(self, sender, blackAndScale=None):
+		# only kill snaps when a document and a tab is open and a glyph layer is open for editing:
+		if Glyphs.font and Glyphs.font.currentTab and Glyphs.font.currentTab.activeLayer():
+			tool = Glyphs.currentDocument.windowController().toolEventHandler()
+			toolCanSnap = tool.respondsToSelector_("cleanSnappers")
+			if toolCanSnap and not self.isKilling:
+				self.isKilling = True
+				tool.cleanSnappers()
+				self.isKilling = False
+	
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
+	
